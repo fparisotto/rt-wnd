c6ee2cfece0dfdcdad2757134eeef04897622146
Fernando Luiz Parisotto <fernando.parisotto@gmail.com>
2023-09-03 14:26:12 -0300

    winit + softbuffer instead of ppm file

--- run-it.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env bash
-
-set -e
-
-TS=$(date +%s)
-time cargo run --release > output_${TS}.ppm && open output_${TS}.ppm
--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,28 @@ mod scene;
 mod sphere;
 mod vec3;
 
+use std::num::NonZeroU32;
+use std::thread;
+
 use crate::camera::Camera;
 use crate::vec3::Vec3;
 
+use rand::prelude::*;
 use rayon::prelude::*;
+use winit::dpi::PhysicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
 
-fn main() {
+use anyhow::{anyhow, Result};
+
+fn main() -> Result<()> {
     // Image
     let aspect_ratio: f64 = 3.0 / 2.0;
     let image_width = 1200;
     let image_height: u32 = (image_width as f64 / aspect_ratio) as u32;
-    let samples_per_pixel = 500;
-    let max_depth = 50;
+    let samples_per_pixel = 100;
+    let max_depth = 10;
 
     // World
     let world = scene::random_scene();
@@ -29,7 +39,7 @@ fn main() {
     let vup = Vec3::new(0.0, 1.0, 0.0);
     let dist_to_focus = 10.0;
     let aperture = 0.1;
-    let cam = Camera::new(
+    let camera = Camera::new(
         &lookfrom,
         &lookat,
         &vup,
@@ -40,37 +50,79 @@ fn main() {
     );
 
     let mut coords: Vec<(u32, u32)> = Vec::new();
-    for y in (0..image_height).rev() {
-        for x in 0..image_width {
+    for x in 0..image_width {
+        for y in 0..image_height {
             coords.push((x, y));
         }
     }
+    // Shuffle the coordinates to make the rendering more interesting
+    coords.shuffle(&mut rand::thread_rng());
 
-    let pixels: Vec<Vec3> = coords
-        .into_par_iter()
-        .map(|coords| {
-            render::render_pixel(
+    let (sender, receive) = std::sync::mpsc::channel::<(Vec3, (u32, u32))>();
+
+    thread::spawn(move || {
+        coords.into_par_iter().for_each(|coords| {
+            let pixel = render::render_pixel(
                 coords,
                 (image_width, image_height),
-                &cam,
+                &camera,
                 &world,
                 max_depth,
                 samples_per_pixel,
-            )
-        })
-        .collect();
+            );
+            sender.send((pixel, coords)).expect("send rendered pixel");
+        });
+    });
 
-    // Render
-    println!("P3\n{} {}\n255\n", image_width, image_height);
+    let event_loop = EventLoop::new();
+    let size = PhysicalSize::new(image_width, image_height);
+    let window = WindowBuilder::new()
+        .with_min_inner_size(size)
+        .with_max_inner_size(size)
+        .with_resizable(false)
+        .build(&event_loop)?;
 
-    for pixel in pixels {
-        println!(
-            "{} {} {}",
-            (256.0 * pixel.x().clamp(0.0, 0.999)) as u32,
-            (256.0 * pixel.y().clamp(0.0, 0.999)) as u32,
-            (256.0 * pixel.z().clamp(0.0, 0.999)) as u32
-        );
-    }
+    let context = unsafe { softbuffer::Context::new(&window) }
+        .map_err(|err| anyhow!("fail to create context, reason: {:?}", err))?;
+    let mut surface = unsafe { softbuffer::Surface::new(&context, &window) }
+        .map_err(|err| anyhow!("fail to create surface, reason: {:?}", err))?;
 
-    eprintln!("Done.");
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+        match event {
+            Event::MainEventsCleared => {
+                let (width, height) = {
+                    let size = window.inner_size();
+                    (size.width, size.height)
+                };
+
+                surface
+                    .resize(
+                        NonZeroU32::new(width).expect("no width"),
+                        NonZeroU32::new(height).expect("no height"),
+                    )
+                    .expect("resize works");
+
+                let mut buffer = surface.buffer_mut().expect("fetch window buffer");
+                for (pixel, (x, y)) in receive.try_iter() {
+                    let red = (256.0 * pixel.x().clamp(0.0, 0.999)) as u32;
+                    let green = (256.0 * pixel.y().clamp(0.0, 0.999)) as u32;
+                    let blue = (256.0 * pixel.z().clamp(0.0, 0.999)) as u32;
+                    let rgb = blue | (green << 8) | (red << 16);
+                    // the y axis is flipped
+                    let y = image_height - y - 1;
+                    let index = (y * image_width + x) as usize;
+                    buffer[index] = rgb;
+                }
+                buffer.present().expect("render not fail");
+            }
+            Event::WindowEvent {
+                window_id,
+                event: WindowEvent::CloseRequested,
+            } if window_id == window.id() => {
+                *control_flow = ControlFlow::Exit;
+            }
+            _ => {}
+        }
+    });
 }
